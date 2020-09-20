9e23d44bf9cfb9628d904e4b879559936a281ddb
constructor-s <bill.shi@mail.utoronto.ca>
2020-09-20 11:58:11 -0400

    Add shebang

--- 1.sh
+++ 1.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 mkdir -p IMAGES && find -regex ^\.\/[0-9]+-[0-9]+\/.+\.jpg$ -exec cp --parents {} IMAGES ';'
 mkdir -p DATA_ANALYSIS && find -regex ^\.\/[0-9]+-[0-9]+\/.+$ ! -name "*.jpg" -exec cp --parents {} DATA_ANALYSIS ';'
