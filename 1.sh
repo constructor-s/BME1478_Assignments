795a77dd796557a4c4f7d1fbe2488cad2b54e79b
constructor-s <bill.shi@mail.utoronto.ca>
2020-09-20 11:00:51 -0400

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,2 @@
+mkdir -p IMAGES && find -regex ^\.\/[0-9]+-[0-9]+\/.+\.jpg$ -exec cp --parents {} IMAGES ';'
+mkdir -p DATA_ANALYSIS && find -regex ^\.\/[0-9]+-[0-9]+\/.+$ ! -name "*.jpg" -exec cp --parents {} DATA_ANALYSIS ';'
