107611d2994e30a28bc11279b1c6f1802305dc76
constructor-s <bill.shi@mail.utoronto.ca>
2020-09-20 12:17:25 -0400

    Add -type f

--- 1.sh
+++ 1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir -p IMAGES && find -regex ^\.\/[0-9]+-[0-9]+\/.+\.jpg$ -exec cp --parents {} IMAGES ';'
-mkdir -p DATA_ANALYSIS && find -regex ^\.\/[0-9]+-[0-9]+\/.+$ ! -name "*.jpg" -exec cp --parents {} DATA_ANALYSIS ';'
+mkdir -p IMAGES && find -type f -regex ^\.\/[0-9]+-[0-9]+\/.+\.jpg$ -exec cp --parents {} IMAGES ';'
+mkdir -p DATA_ANALYSIS && find -type f -regex ^\.\/[0-9]+-[0-9]+\/.+$ ! -name "*.jpg" -exec cp --parents {} DATA_ANALYSIS ';'
